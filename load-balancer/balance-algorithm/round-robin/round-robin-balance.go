ee798ec9a7412132eaa49be50e6ff489cf3883a8
amitiwary999 <tiwarya9@gmail.com>
2024-02-21 22:35:22 +0530

    wip health check for the servers

--- load-balancer/balance-algorithm/round-robin/round-robin-balance.go
+++ load-balancer/balance-algorithm/round-robin/round-robin-balance.go
@@ -4,7 +4,9 @@ import (
 	lb "balanceload/load-balancer"
 	proxy "balanceload/load-balancer/proxy"
 	"net/http"
+	"strconv"
 	"sync/atomic"
+	"time"
 )
 
 type backendServer struct {
@@ -13,18 +15,23 @@ type backendServer struct {
 }
 
 type roundRobin struct {
-	counter uint64
-	urls    []*backendServer
+	counter          uint64
+	urls             []*backendServer
+	backendServerMap map[string]*backendServer
 }
 
 func NewRoundRobin(config *lb.Config, proxyFunc proxy.ProxyFunc) *roundRobin {
+	rr := &roundRobin{}
 	var backendServers []*backendServer
-	for _, b := range config.Backends {
-		backendServers = append(backendServers, &backendServer{url: proxyFunc(b.URL), isServerAlive: true})
+	for i, b := range config.Backends {
+		mapKey := b.URL + strconv.Itoa(i)
+		b := &backendServer{url: proxyFunc(b.URL), isServerAlive: true}
+		rr.backendServerMap[mapKey] = b
+		backendServers = append(backendServers, b)
 	}
-	return &roundRobin{
-		urls: backendServers,
-	}
+	rr.urls = backendServers
+	go rr.healthChecker(config)
+	return rr
 }
 
 func (r *roundRobin) serve(w http.ResponseWriter, req *http.Request) {
@@ -35,4 +42,21 @@ func (r *roundRobin) serve(w http.ResponseWriter, req *http.Request) {
 func (r *roundRobin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	r.serve(w, req)
+}
+
+func (r *roundRobin) healthChecker(config *lb.Config) {
+	for {
+		time.Sleep(time.Duration(30 * time.Second))
+		r.serverHealthCheck(config)
+	}
+}
+
+func (r *roundRobin) serverHealthCheck(config *lb.Config) {
+	for i, b := range config.Backends {
+		mapKey := b.URL + strconv.Itoa(i)
+		ok := lb.IsServerAlive(b.URL)
+		if !ok && r.backendServerMap[mapKey].isServerAlive {
+			r.backendServerMap[mapKey].isServerAlive = false
+		}
+	}
 }
