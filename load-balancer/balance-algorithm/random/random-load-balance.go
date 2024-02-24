3918133838e498800d28e4625f89afadc99cf6e6
amitiwary999 <tiwarya9@gmail.com>
2024-02-24 21:39:43 +0530

    health check of server for random load balance

--- load-balancer/balance-algorithm/random/random-load-balance.go
+++ load-balancer/balance-algorithm/random/random-load-balance.go
@@ -5,27 +5,75 @@ import (
 	proxy "balanceload/load-balancer/proxy"
 	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
+type backendServer struct {
+	url           proxy.IProxy
+	isServerAlive bool
+}
+
 type random struct {
-	urls []proxy.IProxy
+	urls             []*backendServer
+	backendServerMap map[string]*backendServer
 }
 
 func NewRandom(config *lb.Config, proxyFunc proxy.ProxyFunc) *random {
-	var urls []proxy.IProxy
-	for _, b := range config.Backends {
-		urls = append(urls, proxyFunc(b.URL))
+	ran := &random{}
+	ran.backendServerMap = make(map[string]*backendServer)
+	var backendServers []*backendServer
+	for i, b := range config.Backends {
+		mapKey := b.URL + strconv.Itoa(i)
+		b := &backendServer{url: proxyFunc(b.URL), isServerAlive: true}
+		ran.backendServerMap[mapKey] = b
+		backendServers = append(backendServers, b)
 	}
-	return &random{
-		urls: urls,
-	}
+	ran.urls = backendServers
+	go ran.healthChecker(config)
+	return ran
 }
 
 func (r *random) serve(w http.ResponseWriter, req *http.Request) {
-	r.urls[rand.Intn(len(r.urls))].ReverseProxy(w, req)
+	r.urls[rand.Intn(len(r.urls))].url.ReverseProxy(w, req)
 }
 
 func (r *random) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	r.serve(w, req)
+}
+
+func (r *random) healthChecker(config *lb.Config) {
+	for {
+		time.Sleep(time.Duration(10 * time.Second))
+		r.serverHealthCheck(config)
+	}
+}
+
+func (r *random) serverHealthCheck(config *lb.Config) {
+	for i, b := range config.Backends {
+		mapKey := b.URL + strconv.Itoa(i)
+		ok := lb.IsServerAlive(b.URL)
+		if !ok && r.backendServerMap[mapKey].isServerAlive {
+			r.backendServerMap[mapKey].isServerAlive = false
+			r.urls = resizeServer(r.backendServerMap[mapKey], r.urls)
+		} else if ok && !r.backendServerMap[mapKey].isServerAlive {
+			r.backendServerMap[mapKey].isServerAlive = true
+			r.urls = append(r.urls, r.backendServerMap[mapKey])
+		}
+	}
+	if len(r.urls) <= 0 {
+		panic("none of the server is live")
+	}
+}
+
+func resizeServer(b *backendServer, bs []*backendServer) []*backendServer {
+	rs := bs
+	for i, bsp := range bs {
+		if bsp == b {
+			rs = append(bs[:i], bs[i+1:]...)
+			break
+		}
+	}
+	return rs
 }
